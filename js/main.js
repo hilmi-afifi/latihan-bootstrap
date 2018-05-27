0c14c63cf3653a91dab7ec91116a22ea0f3a40e1
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2018-05-27 23:49:47 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// setelah discroll, navbar menjadi fixed on top
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
